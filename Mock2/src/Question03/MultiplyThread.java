92196a0a8137ef2497556ead59e1b1740efceb34
dlx996 <72247994+dlx996@users.noreply.github.com>
2020-10-02 15:32:10 +0530

    Performance improved
    Performance improved, Error Fixed

--- Mock2/src/Question03/MultiplyThread.java
+++ Mock2/src/Question03/MultiplyThread.java
@@ -1,16 +1,16 @@
 package Question03;
 
-pub class MultiplyThread implements Runnable {
+public class MultiplyThread implements Runnable {
 
-	private Object lock
-	private int start
-	private int range
+	private Object lock;
+	private int start;
+	private int range;
 
 	public MultiplyThread(Object lock, int start, int range) {
-		super()
-		this.lock = lock
-		this.start = start
-		this.range = range
+		super();
+		this.lock = lock;
+		this.start = start;
+		this.range = range;
 	}
 
 	@Override
@@ -19,24 +19,16 @@ pub class MultiplyThread implements Runnable {
 	}
 
 	void multiplyNumbers(Object lock, int start, int range) {
-
 		synchronized (lock) {
-
 			for (int x = start; x <= range; x++) {
-
-				int mul = x * x
+				int mul = x * x;
 				System.out.println(Thread.currentThread().getName() + " => " + x + " x " + x + " = " + mul);
-
-				lock.notify()
-
+				lock.notify();
 				try {
-
-					Thread.sleep(1000)
-					lock.wait()
-
+					Thread.sleep(1000);
+					lock.wait();
 				} catch (Exception e) {
-
-					e.printStackTrace()
+					e.printStackTrace();
 				}
 			}
 		}
